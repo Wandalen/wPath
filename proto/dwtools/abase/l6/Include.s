90e47a59579e2e3727fa54f90b21d24ce92e6a0f
Dmytro <dm.vict.kr@gmail.com>
2020-04-21 09:08:31 +0300

    Extend module, add dependency, increase module level, description

--- proto/dwtools/abase/l6/Include.s
+++ proto/dwtools/abase/l6/Include.s
@@ -3,11 +3,17 @@
 
 'use strict';
 
+/**
+ * Tools to manipulate path. Aggregates modules PathBasic and PathTools. Module Path leverages parsing, joining, extracting, normalizing, nativizing, resolving paths... Use the module to get uniform experience from playing with paths on different platforms.
+ * @module Tools/base/Path
+*/
+
 if( typeof module !== 'undefined' && module !== null )
 {
   let _ = require( '../../Tools.s' );
 
   _.include( 'wPathBasic' );
+  _.include( 'wPathTools' );
 }
 
 let _global = _global_;
